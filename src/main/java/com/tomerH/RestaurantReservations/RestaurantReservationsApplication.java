fb1e8e3f09001d5988f2f27d4a2864589ee269bf
Tomer2H <tomer2b0525@gmail.com>
2024-05-31 15:38:05 +0300

    added on start adding of roles and admin credentials for testing

--- src/main/java/com/tomerH/RestaurantReservations/RestaurantReservationsApplication.java
+++ src/main/java/com/tomerH/RestaurantReservations/RestaurantReservationsApplication.java
@@ -1,7 +1,17 @@
 package com.tomerH.RestaurantReservations;
 
+import com.tomerH.RestaurantReservations.Beans.Role;
+import com.tomerH.RestaurantReservations.Beans.UserCredentials;
+import com.tomerH.RestaurantReservations.Repositories.RoleRepository;
+import com.tomerH.RestaurantReservations.Repositories.UserCredentialsRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @SpringBootApplication
 public class RestaurantReservationsApplication {
@@ -10,4 +20,21 @@ public class RestaurantReservationsApplication {
 		SpringApplication.run(RestaurantReservationsApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner run(RoleRepository roleRepository, UserCredentialsRepository credentialsRepository, PasswordEncoder passwordEncoder){
+		return args -> {
+			if (roleRepository.findByAuthority("ADMIN").isPresent()) return;
+			Role adminRole = roleRepository.save(new Role("ADMIN"));
+			roleRepository.save(new Role("MEMBER"));
+			roleRepository.save(new Role("RESTAURANT"));
+			roleRepository.save(new Role("VISITOR"));
+
+			Set<Role> roles = new HashSet<>();
+			roles.add(adminRole);
+
+			UserCredentials admin = new UserCredentials(1,"admin",passwordEncoder.encode("Admin1234"),roles);
+
+			credentialsRepository.save(admin);
+		};
+	}
 }
