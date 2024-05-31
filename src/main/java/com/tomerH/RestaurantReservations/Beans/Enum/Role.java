c232c92ec09121e0f9074073e33a6dff9e92a2a4
Tomer2H <tomer2b0525@gmail.com>
2024-05-31 15:30:53 +0300

    Updated roles to be in database instead of enum in backend.

--- src/main/java/com/tomerH/RestaurantReservations/Beans/Enum/Role.java
+++
@@ -1,6 +0,0 @@
-package com.tomerH.RestaurantReservations.Beans.Enum;
-
-public enum Role {
-    ADMINISTRATOR,MEMBER,RESTAURANT,VISITOR;
-
-}
---
+++ src/main/java/com/tomerH/RestaurantReservations/Beans/Role.java
@@ -0,0 +1,53 @@
+package com.tomerH.RestaurantReservations.Beans;
+
+import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+
+@Entity
+@Table(name="roles")
+public class Role implements GrantedAuthority {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id")
+    private int roleId;
+    private String authority;
+
+    public Role() {
+        super();
+    }
+
+    public Role(String authority) {
+        this.authority = authority;
+    }
+
+    public Role(int roleId, String authority) {
+        this.roleId = roleId;
+        this.authority = authority;
+    }
+
+    public int getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(int roleId) {
+        this.roleId = roleId;
+    }
+
+    public void setAuthority(String authority) {
+        this.authority = authority;
+    }
+
+    @Override
+    public String getAuthority() {
+        return this.authority;
+    }
+
+    @Override
+    public String toString() {
+        return "Role{" +
+                "roleId=" + roleId +
+                ", authority='" + authority + '\'' +
+                '}';
+    }
+}
